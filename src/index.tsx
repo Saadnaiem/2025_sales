7af1f028040b74c4a83e1222b37c63e07452cb69
Saadnaiem <s_pharma2008@yahoo.com>
2025-12-24 11:18:58 +0300

    Fix TS error in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename={(import.meta as any).env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
