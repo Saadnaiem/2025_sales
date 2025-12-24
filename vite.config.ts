fc9e1710799255dadbd62f20cb82806a8d0e4fa2
Saadnaiem <s_pharma2008@yahoo.com>
2025-12-24 11:07:02 +0300

    Fix localhost config

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,28 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/2025_sales/',
-  build: {
-    rollupOptions: {
-      // Add 'external' to tell Rollup not to bundle these packages.
-      // They are provided at runtime by the importmap in index.html.
-      external: ['jspdf', 'jspdf-autotable'],
-      output: {
-        manualChunks(id) {
-          // Group large vendor libraries into a separate chunk to improve performance
-          // and resolve the chunk size warning.
-          if (id.includes('node_modules')) {
-            if (id.includes('recharts') || id.includes('papaparse')) {
-              return 'vendor';
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: command === 'serve' ? '/' : '/2025_sales/',
+    build: {
+      rollupOptions: {
+        // Add 'external' to tell Rollup not to bundle these packages.
+        // They are provided at runtime by the importmap in index.html.
+        external: ['jspdf', 'jspdf-autotable'],
+        output: {
+          manualChunks(id: string) {
+            // Group large vendor libraries into a separate chunk to improve performance
+            // and resolve the chunk size warning.
+            if (id.includes('node_modules')) {
+              if (id.includes('recharts') || id.includes('papaparse')) {
+                return 'vendor';
+              }
             }
-          }
+          },
         },
       },
     },
-  },
+  };
+  return config;
 })
